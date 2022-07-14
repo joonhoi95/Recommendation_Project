9ddddf5b57420f1d8e599dc1c255d7a4ceef7851
Junho Park <joonhoi95@gmail.com>
2022-07-14 13:00:55 +0900

    test project

---
+++ content_based.py
@@ -0,0 +1,99 @@
+import os
+import re
+import nltk
+import requests
+import warnings
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+#matplotlib inline
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+import seaborn as sns
+
+from nltk.corpus import stopwords
+nltk.download("stopwords")
+
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
+from PIL import Image
+warnings.filterwarnings('ignore')
+
+books = pd.read_csv('C:/Users/junho park/Desktop/Python/Reco_Engine/res/Preprocessed_data.csv')
+
+book_data = pd.read_csv('C:/Users/junho park/Desktop/Python/Reco_Engine/res/Preprocessed_data.csv')
+
+df = books.copy()
+df.dropna(inplace=True)
+df.reset_index(drop=True, inplace=True)
+
+df.drop(columns = ['Unnamed: 0','location','isbn',
+                   'img_s','img_m','city','age',
+                   'state','Language','country',
+                   'year_of_publication'],axis=1,inplace = True) #remove useless cols
+
+df.drop(index=df[df['Category'] == '9'].index, inplace=True) #remove 9 in category
+
+df.drop(index=df[df['rating'] == 0].index, inplace=True) #remove 0 in rating
+
+#문자열(_)제외하고 반복하면서 삭제
+df['Category'] = df['Category'].apply(lambda x: re.sub('[\W_]+',' ',x).strip())
+
+
+def content_based_recommender(book_title):
+    book_title = str(book_title)
+    if book_title in df['book_title'].values:
+        rating_counts = pd.DataFrame(df['book_title'].value_counts())
+        rare_books = rating_counts[rating_counts['book_title'] <= 100].index
+        common_books = df[~df['book_title'].isin(rare_books)]
+
+        if book_title in rare_books:
+
+            random = pd.Series(common_books['book_title'].unique()).sample(2).values
+            print('There are no recommendations for this book')
+            print('Try: \n')
+            print('{}'.format(random[0]), '\n')
+            print('{}'.format(random[1]), '\n')
+
+        else:
+
+            common_books = common_books.drop_duplicates(subset=['book_title'])
+            common_books.reset_index(inplace=True)
+            common_books['index'] = [i for i in range(common_books.shape[0])]
+            target_cols = ['book_title', 'book_author', 'publisher', 'Category']
+            common_books['combined_features'] = [' '.join(common_books[target_cols].iloc[i,].values) for i in
+                                                 range(common_books[target_cols].shape[0])]
+            cv = CountVectorizer()
+            count_matrix = cv.fit_transform(common_books['combined_features'])
+            cosine_sim = cosine_similarity(count_matrix)
+            index = common_books[common_books['book_title'] == book_title]['index'].values[0]
+            sim_books = list(enumerate(cosine_sim[index]))
+            sorted_sim_books = sorted(sim_books, key=lambda x: x[1],
+                                      reverse=True)[1:11]
+
+            books = []
+            for i in range(len(sorted_sim_books)):
+                books.append(common_books[common_books['index'] == sorted_sim_books[i][0]]['book_title'].item())
+
+            fig, axs = plt.subplots(1, 10, figsize=(18, 5))
+            fig.suptitle('You may also like these books', size=22)
+            for i in range(len(books)):
+                url = common_books.loc[common_books['book_title'] == books[i], 'img_l'][:1].values[0]
+                im = Image.open(requests.get(url, stream=True).raw)
+                axs[i].imshow(im)
+                axs[i].axis("off")
+                axs[i].set_title('Rating: {}'.format(round(df[df['book_title'] == books[i]]['rating'].mean(), 1)) \
+                                 + '\n' + 'title: {}'.format(common_books.loc[common_books['book_title'] == books[i], 'book_title'][:1].values[0]) \
+                                 + '\n' + 'title: {}'.format(common_books.loc[common_books['book_title'] == books[i], 'Category'][:1].values[0]) ,
+                                 y=-0.3,
+                                 color="blue",
+                                 fontsize=12)
+                fig.show()
+
+    else:
+
+        print('Cant find book in dataset, please check spelling')
+
+#sns.heatmap(cosine_sim, xticklabels=common_books['combined_features'], yticklabels=common_books['combined_features'], cmap='viridis')
+#plt.show()
